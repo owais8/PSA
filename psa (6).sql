19dc5444f2ebf7d88f69ca63add0099ec2bb1853
owaisorakzai <84202122+owaisorakzai@users.noreply.github.com>
2023-09-19 00:56:20 +0500

    aaasa

--- psa (6).sql
+++ psa (6).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 18, 2023 at 12:05 AM
+-- Generation Time: Sep 18, 2023 at 09:55 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -43,13 +43,8 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`id`, `username`, `email`, `password`, `name`, `password_new`, `reset_token`, `role`) VALUES
-(8, 'owais', 'owaisorakzai77@gmail.com', '$2y$10$bcYvJmi4x2JtTPZn9Zkh6Oq55f.oXl6t5DZyNv5B.JTa98zK.T2bK', 'Owais Orakzai', 'done', '', 'admin'),
-(9, 'aslam', 'aslam@gmail.com', '$2y$10$pNxC4FpXnDR/YkBMNcfsFuBRfPW3s.WV1PU.cSP.QfxIXZulodcBG', 'aslam', 'done', '', 'owner'),
-(11, 'aa', 'o@gmail.com', '$2y$10$bcYvJmi4x2JtTPZn9Zkh6Oq55f.oXl6t5DZyNv5B.JTa98zK.T2bK', 'Owais Orakzai', 'Change', '', 'admin'),
-(12, 'dddd', 'asdas@gmail.com', '$2y$10$bcYvJmi4x2JtTPZn9Zkh6Oq55f.oXl6t5DZyNv5B.JTa98zK.T2bK', 'Owais Orakzai', 'Change', '', 'admin'),
-(13, 'as', 'asda@gmail.com', '$2y$10$bcYvJmi4x2JtTPZn9Zkh6Oq55f.oXl6t5DZyNv5B.JTa98zK.T2bK', 'aslam', 'Change', '', 'admin'),
-(14, 'asdcc', 'daaa@gmai.com', '$2y$10$bcYvJmi4x2JtTPZn9Zkh6Oq55f.oXl6t5DZyNv5B.JTa98zK.T2bK', 'dadas@gmail.com', 'Change', '', 'admin'),
-(15, 'addddddd', 'dasdas@gmail.com', '$2y$10$8Bxwg2kaopS9rBczzLDKVOpz07024ucF42jObUovqwB8TNY/Lt6ie', 'Owais Orakzai', 'done', '', 'owner');
+(9, 'admin', 'salman121321@gmail.com', '$2y$10$vIKUm7AgnVmjc/t7mZPKXuawaCmPIncCiy9PZAY4XriB.TWqucOAO', 'aslam', 'done', '', 'owner'),
+(18, 'owais', 'owaisorakzai77@gmail.com', '$2y$10$gQIu8b/NGiO1mnbnu46ZaOubTWTOJ/5y4j/mx.rPdWsg9PBTNRQGC', 'Owais Orakzai', 'done', '', 'admin');
 
 -- --------------------------------------------------------
 
@@ -126,7 +121,9 @@ INSERT INTO `order_details` (`id`, `order_id`, `qty`, `cardYear`, `brand`, `card
 (8, 26, 22, '2022', '', '', '', '', 1000.00, '2023-09-15 14:35:50'),
 (9, 27, 10, '2022', '', '', '', '', 1000.00, '2023-09-15 14:57:10'),
 (10, 27, 50, '2023', '', '', '', '', 1000.00, '2023-09-15 14:57:21'),
-(11, 28, 11, '2022', '', '', '', '', 1111.00, '2023-09-17 14:52:00');
+(11, 28, 11, '2022', '', '', '', '', 1111.00, '2023-09-17 14:52:00'),
+(12, 29, 22, '2022', '', '', '', '', 1000.00, '2023-09-17 22:40:47'),
+(13, 30, 22, '2022', '', '', '', '', 2000.00, '2023-09-18 19:51:45');
 
 -- --------------------------------------------------------
 
@@ -175,7 +172,9 @@ INSERT INTO `submissions` (`id`, `first_name`, `last_name`, `email`, `phone_numb
 (25, 'owaisorakzai77@gmail.com', 'Qarni', 'owaisorakzai77@gmail.com', '+923111232327', 'Bobs Hostel, Main Hamza Rd, F11, Islamabad', 'Hhhh', 'asdasd', 'Islamabad', '', '', 88.00, 76.00, 12.00, 'PSA Bulk 1980-present- $19 per card - Max Declared Value: $499 - Est. Turnaround : 60 Business Days|', 'PSA', 4, 1, '2023-09-09 01:26:35', 0, 'Awaiting Card List', '', 'old', ''),
 (26, 'Owais', 'Qarni', 'owaisorakzai77@gmail.com', '+923111232327', 'Bobs Hostel, Main Hamza Rd, F11, Islamabad', 'ss', 'dd', 'Islamabad', '', '', 484.00, 418.00, 66.00, 'PSA Bulk 1980-present- $19 per card - Max Declared Value: $499 - Est. Turnaround : 60 Business Days|', 'PSA', 22, 3, '2023-09-15 14:35:39', 48, 'Awaiting Card List', '', 'old', 'paid'),
 (27, 'asds', 'asd', 'owaisorakzai77@gmail.com', '+923111232327', 'Bobs Hostel, Main Hamza Rd, F11, Islamabad', 'asd', 'asdas', 'Islamabad', '', '', 484.00, 418.00, 66.00, 'PSA Bulk 1980-present- $19 per card - Max Declared Value: $499 - Est. Turnaround : 60 Business Days|', 'PSA', 22, 3, '2023-09-15 14:57:00', 6, 'Awaiting Card List', '', 'old', 'unpaid'),
-(28, 'owais', 'Qarni', 'owaisorakzai77@gmail.com', '+923111232327', 'Bobs Hostel, Main Hamza Rd, F11, Islamabad', 'Hhhh', 'asdasd', 'Islamabad', '', '', 3663.00, 3630.00, 33.00, 'PSA Super Express- $330 per card - Max Declared Value $4,999 - Est. Turnaround: 3 Business Days|330', 'PSA', 11, 14, '2023-09-17 14:51:46', 11, 'Awaiting Card List', '', 'old', 'unpaid');
+(28, 'owais', 'Qarni', 'owaisorakzai77@gmail.com', '+923111232327', 'Bobs Hostel, Main Hamza Rd, F11, Islamabad', 'Hhhh', 'asdasd', 'Islamabad', '', '', 3663.00, 3630.00, 33.00, 'PSA Super Express- $330 per card - Max Declared Value $4,999 - Est. Turnaround: 3 Business Days|330', 'PSA', 11, 14, '2023-09-17 14:51:46', 11, 'Awaiting Card List', '', 'old', 'paid'),
+(29, 'Owais', 'Orakzai', 'owaisorakzai77@gmail.com', '+923111232327', 'Bobs Hostel, Main Hamza Rd, F11, Islamabad', 'asdas', 'asasd', 'Islamabad', '', '', 532.00, 475.00, 57.00, 'PSA Value 1979-older- $25 per card - Max Declared Value: $499 - Est. Turnaround: 65 Business Days|25', 'PSA', 19, 17, '2023-09-17 22:40:05', 11, 'Awaiting Card List', '', 'old', 'paid'),
+(30, 'Owais', 'Orakzai', 'owaisorakzai99@gmail.com', '111111', 'Bobs Hostel, Main Hamza Rd, F11, Islamabad', 'Hhhh', 'asasd', 'Islamabad', '', '', 2322.00, 2160.00, 162.00, 'PSA Value Plus - $40 per card - Max Declared Value: $499 - Est. Turnaround: 20 Business Days|40', 'PSA', 54, 18, '2023-09-18 19:51:33', 12, 'Awaiting Card List', '', 'new', 'unpaid');
 
 -- --------------------------------------------------------
 
@@ -202,8 +201,9 @@ INSERT INTO `users` (`id`, `username`, `email`, `password`, `verification_token`
 (3, 'karan', 'k@gmail.com', '$2y$10$8pkQgRmKkXibbs13y2rM8eVdJF9rA2ZN5Pzg8tODJS0XdY3OhXxca', '', 'unverified'),
 (9, 'admin77', 'owaisor@gmail.com', '$2y$10$mnAdoelRzS368ehvaK6/8u1dT3aUKzdIWZERRvDZ5llDcffHYr8vW', '', 'unverified'),
 (11, 'ZZ', 'z@gmail.com', '$2y$10$eZZt.VTt9T4gEbh0Ir7r4.MCLkPrYrAh.waM1ptY8vlwMu2Iz3coC', '', 'unverified'),
-(14, 'ff', 'owaisorakzai99@gmail.com', '$2y$10$Mm2484FtL/9oSbQizT.KnurukEgd2t1DVk/6YXURF5tEoZHKctQx6', 'dc8819409dc888ba8215d587ae1743474270e38812873143a5381b75a4821b23', 'verified'),
-(15, 'karan1111111111111', 'adsd@gmail.com', '$2y$10$u5Q1KgkJNaaxPycXAvDQVOq/Js5qs1DSkV5yjgnRhuihZRQ/nk8Cy', 'a140420939bfdc9bafde4785801708284678261ec6e5debd8f53e3582b729f10', 'verified');
+(14, 'ff', 'owaisorakzai99000@gmail.com', '$2y$10$Mm2484FtL/9oSbQizT.KnurukEgd2t1DVk/6YXURF5tEoZHKctQx6', 'dc8819409dc888ba8215d587ae1743474270e38812873143a5381b75a4821b23', 'verified'),
+(17, 'owais1234567', 'owaisorakzai99@gmail.com', '$2y$10$Th6UOSPGUMSISoNYxpk7vuqYuL2Xep5jzl3rsyLfqdVnWKAfOWXSu', '6d29be3679c82056acf8241fb049537b2bb5b7d58844566ac22832b53610dfc7', 'verified'),
+(18, 'zmn', 'zmn@gmail.com', '$2y$10$2VqOjm7XrEpAcdYjqO6uQeyBnBFKfvAxr3FTZeKVaLCIChQ7FCVvW', '8fb3a672d42aa91b890dbe160e049d43df89da5b5387477c153e109575f020d4', 'verified');
 
 -- --------------------------------------------------------
 
@@ -221,7 +221,7 @@ CREATE TABLE `website` (
 --
 
 INSERT INTO `website` (`id`, `home_text`) VALUES
-(1, 'You are more than welcome to submit different service levels in the same package to us (ex: PSA bulk & SGC regular).\r\n\r\nHowever each service level needs a different order placed on our site, as they would come back at different times.\r\n\r\nSo you would place your order for (ex: 20 cards bulk, complete shipping and place order) then place a new order for the other service(s) you need.\r\n\r\nYou would then print out your 2 packing slips, put each form with the cards going each service level in the 1 package.\r\n\r\nThis allows you to get status updates on each individual order. If submitting just 1 order type (ex: PSA bulk) simply put your packing slip with the cards and you are good to go!\r\n\r\nGood luck on your submission, and thank you for choosing kksportscards.\r\n\r\n');
+(1, 'You are more than welcome to submit different service levels in the same package to us (ex: PSA bulk & SGC regular).\r\n\r\nHowever each service level needs a different order placed on our site, as they would come back at different times.\r\n\r\nSo you would place your order for (ex: 20 cards bulk, complete shipping and place order) then place a new order for the other service(s) you need.\r\n\r\nYou would then print out your 2 packing slips, put each form with the cards going each service level in the 1 package.\r\n\r\nThis allows you to get status updates on each individual order. If submitting just 1 order type (ex: PSA bulk) simply put your packing slip with the cards and you are good to go!\r\n\r\n');
 
 --
 -- Indexes for dumped tables
@@ -284,7 +284,7 @@ ALTER TABLE `website`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `orders`
@@ -302,19 +302,19 @@ ALTER TABLE `orders_psa`
 -- AUTO_INCREMENT for table `order_details`
 --
 ALTER TABLE `order_details`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `submissions`
 --
 ALTER TABLE `submissions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `website`
